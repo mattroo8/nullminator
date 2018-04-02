e8a54c0fc9fd8140963da6fd3883157685b3de5b
Matt Rooney <matthew.rooney8@gmail.com>
2018-04-02 21:22:19 +0100

    removing "null" works on 'null'

--- src/main/groovy/ListNullminatorExtension.groovy
+++ src/main/groovy/ListNullminatorExtension.groovy
@@ -1,14 +1,14 @@
 class ListNullminatorExtension {
 
     /**
-     Removes any and all null values or strings containing "null" or 'null'.
+     Removes all null values or strings containing "null".
      @param self The list the method was called on
      @return List - The modified list
      */
     static List removeAllTheNulls(final List self){
         List target = self
         target = target - null
-        target.removeAll(["null", 'null'])
+        target.removeAll(["null"])
         return target
     }
 }
